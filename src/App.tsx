caffcc69b9d6d415fd4dc605f4e3204636522218
visvoker <s2089533@gmail.com>
2025-04-09 22:33:50 +0800

    Complete Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import Footer from "./components/layout/Footer"
+import Container from "./components/layout/Container"
+import HashtagList from "./components/layout/HashtagList"
 
 
 function App() {
 
   return (
-    <>
-      <div>
-        Hello
-      </div>
-    </>
+    <div className="app">
+      <Footer />
+
+      <Container />
+
+      <HashtagList />
+    </div>
   )
 }
 
---
+++ src/components/layout/HashtagList.tsx
@@ -0,0 +1,15 @@
+export default function HashtagList() {
+  return (
+    <ul className="hashtags">
+      <li>
+        <button>#Hello</button>
+      </li>
+      <li>
+        <button>#ayo</button>
+      </li>
+      <li>
+        <button>#McDonald's</button>
+      </li>
+    </ul>
+  )
+}
