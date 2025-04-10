6bd6e1e56881f648ca58a018dd939ecda064430f
visvoker <s2089533@gmail.com>
2025-04-10 18:32:24 +0800

    TypeScript For Props

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,27 +1,30 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons"
+import FeedbackItem from "./FeedbackItem";
+
+const feedbackItems = [
+  {
+    upvoteCount: 593,
+    badgeLetter: "B",
+    companyName: "ByteGard",
+    text: "test test",
+    daysAgo: 4,
+  },
+  {
+    upvoteCount: 563,
+    badgeLetter: "S",
+    companyName: "Starbucks",
+    text: "test Starbucks",
+    daysAgo: 3,
+  },
+]
 
 export default function FeedbackList() {
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <TriangleUpIcon />
-          <span>593</span>
-        </button>
-
-        <div>
-          <p>B</p>
-        </div>
-
-        <div>
-          <p>ByteGrad</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro sapiente aspernatur accusamus explicabo consectetur cum.
-          </p>
-        </div>
-
-        <p>4d</p>
-      </li>
+      {feedbackItems.map((feedbackItem) => {
+        return (
+          <FeedbackItem feedbackItem={feedbackItem} />
+        )
+      })}
     </ol>
   )
 }
