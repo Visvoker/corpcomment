7de1f190c6a71eae7150272b47519cbf999b737f
visvoker <s2089533@gmail.com>
2025-04-11 12:07:49 +0800

    Fetch Data & Set State & Loading Spinner

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,28 +1,33 @@
-import FeedbackItem from "./FeedbackItem";
+import { useEffect, useState } from "react";
 
-const feedbackItems = [
-  {
-    upvoteCount: 593,
-    badgeLetter: "B",
-    companyName: "ByteGard",
-    text: "test test",
-    daysAgo: 4,
-  },
-  {
-    upvoteCount: 563,
-    badgeLetter: "S",
-    companyName: "Starbucks",
-    text: "test Starbucks",
-    daysAgo: 3,
-  },
-]
+import FeedbackItem from "./FeedbackItem";
+import Spinner from "./spinner";
 
 export default function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(
+      'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+  }, []);
+
   return (
     <ol className="feedback-list">
+      {isLoading ? <Spinner /> : null}
+
       {feedbackItems.map((feedbackItem) => {
         return (
-          <FeedbackItem feedbackItem={feedbackItem} />
+          <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
         )
       })}
     </ol>
