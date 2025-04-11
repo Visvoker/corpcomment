1d7a366eaefdb2e8e7dbbb650f37785f562637ca
visvoker <s2089533@gmail.com>
2025-04-11 12:28:25 +0800

    Top 3 Errors when Fetching data

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -2,21 +2,32 @@ import { useEffect, useState } from "react";
 
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     setIsLoading(true)
+
     fetch(
-      'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks'
+      'https://bytegrad.com/course-assets/projects/corpcomment/api/asdasd'
     )
       .then((response) => {
+        if (!response.ok) {
+          throw new Error();
+        }
         return response.json();
       })
       .then((data) => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        // network error,not 2xx response, or JSON parsing error
+        setErrorMessage("Something went wrong.");
         setIsLoading(false);
       })
   }, []);
@@ -24,6 +35,8 @@ export default function FeedbackList() {
   return (
     <ol className="feedback-list">
       {isLoading ? <Spinner /> : null}
+
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
 
       {feedbackItems.map((feedbackItem) => {
         return (
